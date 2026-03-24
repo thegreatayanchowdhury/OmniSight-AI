67ad13d0b3141b3f9eafb889ea05c22e11145af1
Ashish0604-AK <ashikmr0604@gmail.com>
2026-03-24 14:40:21 +0530

    Updated Loading UI for different devices

--- omnisight-frontend/src/Loading.jsx
+++ omnisight-frontend/src/Loading.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
-// import introVideo from "public/Intro.mp4";
 
 const Loading = ({ onFinish }) => {
   const [fadeOut, setFadeOut] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 640);
   const finishedRef = useRef(false);
 
   const finishLoading = () => {
@@ -17,30 +17,38 @@ const Loading = ({ onFinish }) => {
   };
 
   useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 640);
+    };
+
     const timer = setTimeout(() => {
       finishLoading();
-    }, 6500);
+    }, 4500);
 
-    return () => clearTimeout(timer);
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      clearTimeout(timer);
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   return (
     <div
-  className={`fixed inset-0 z-[9999] overflow-hidden bg-black transition-opacity duration-700 ${
-    fadeOut ? "opacity-0 pointer-events-none" : "opacity-100"
-  }`}
->
-  <video
-    src="Intro.mp4"
-    autoPlay
-    muted
-    playsInline
-    preload="auto"
-    onEnded={finishLoading}
-    className="absolute inset-0 h-full w-full object-cover"
-    style={{ objectPosition: " center center" }}
-  />
-
+      className={`fixed inset-0 z-[9999] overflow-hidden bg-black transition-opacity duration-700 ${
+        fadeOut ? "opacity-0 pointer-events-none" : "opacity-100"
+      }`}
+    >
+      <video
+        src={isMobile ? "/Intro_mobile.mp4" : "/Intro.mp4"}
+        autoPlay
+        muted
+        playsInline
+        preload="auto"
+        onEnded={finishLoading}
+        className="absolute inset-0 h-full w-full object-cover"
+        style={{ objectPosition: "center center" }}
+      />
     </div>
   );
 };
